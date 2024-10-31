0a69ce02455f357b1618d911fa310729dd42da4d
Анастасия <144614420+Anastasiaae@users.noreply.github.com>
2024-10-31 23:38:18 +0300

    task2.1.py

--- task2.1.py
+++ task2.1.py
@@ -2,6 +2,7 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
+month = 0
 
 while True:
     money_capital += salary
